ad67f276b9f9e0798f6755cd7c011200b69a7340
kalanaashen <northernwolf1472@gmail.com>
2026-03-26 00:50:20 +0530

    add models

--- src/social/models.py
+++ src/social/models.py
@@ -1,8 +1,12 @@
 from django.db import models
 
-# Create your models here.
+
+
 class User(models.Model):
     
+    username = models.CharField(max_length=50)
+    email = models.EmailField()
+
     
     def __str__(self):
         return self.username
@@ -10,5 +14,47 @@ class User(models.Model):
     
 class Profile(models.Model):
     
-    user=models.ForiegnKey(User,on_delete=models.CASCADE,null=True,blank=True,related_name="user")
+    user=models.OneToOneField(User,on_delete=models.CASCADE,null=True,blank=True,related_name="profiles")
+    bio=models.CharField(max_length=80,null=True,blank=True)
+    avatar = models.ImageField(upload_to="avatar/", null=True, blank=True)
     
+    def __str__(self):
+        return self.user
+    
+    
+    
+class Post(models.Model):
+    user=models.ForeignKey(User,on_delete=models.CASCADE,null=False,related_name="posts")
+    image=models.ImageField(upload_to="posts/")
+    caption=models.CharField(max_length=50)
+    created_at=models.DateTimeField(auto_now_add=True)
+    
+
+    def __str__(self):
+        return f"{self.user}-{self.image}"
+
+class Like(models.Model):
+    user=models.ForeignKey(User,on_delete=models.CASCADE,related_name="likes")
+    post=models.ForeignKey(Post,on_delete=models.CASCADE,related_name="likes")
+    
+    
+    def __str__(self):
+        return f"{self.user}-{self.post}"
+    
+    
+class Follower(models.Model):
+    follower=models.ForeignKey(User,on_delete=models.CASCADE,related_name="followings")
+    following=models.ForeignKey(User,on_delete=models.CASCADE,related_name="followers")
+    
+       
+    def __str__(self):
+        return f"{self.follower}- {self.following}"
+    
+class Comment(models.Model):
+    user=models.ForeignKey(User,on_delete=models.CASCADE,related_name="comments")
+    post=models.ForeignKey(Post,on_delete=models.CASCADE,related_name="comments")
+    text=models.CharField(max_length=100,null=True,blank=False)
+    created_at=models.DateTimeField(auto_now_add=True)
+    
+    def __str__(self):
+        return f"{self.user}-{self.post}-{self.text}"
